5314a12a339e9f71c41746b91eed145594d72174
Christian McDonald <christian.mcdonald@utexas.edu>
2019-03-11 15:58:34 -0500

    extrapolating html into src

--- gulpfile.js
+++ gulpfile.js
@@ -10,12 +10,19 @@ gulp.task('sass', function() {
         .pipe(browserSync.stream());
 });
 
-// Move the javascript files into our /src/js folder
+// Move the javascript files into our /docs/js folder
 gulp.task('js', function() {
     return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
         .pipe(gulp.dest("docs/js"))
         .pipe(browserSync.stream());
 });
+
+// Move the index.html file from src to docs
+gulp.task('html', function() {
+    return gulp.src(['src/html/*.html'])
+        .pipe(gulp.dest("docs/"))
+        .pipe(browserSync.stream());
+})
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
@@ -25,8 +32,8 @@ gulp.task('serve', ['sass'], function() {
     });
 
     gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
-    gulp.watch("docs/*.html").on('change', browserSync.reload);
+    gulp.watch("src/html/*.html", ['html']);
 });
 
-gulp.task('default', ['js','sass']);
-gulp.task('dev', ['js','serve']);
+gulp.task('default', ['js','sass', 'html']);
+gulp.task('dev', ['js','html','serve']);
